3ef9922b17ff5f1589b3bc5702a90f00f7e2acdf
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2024-04-01 23:29:30 -0500

    se añaden cambios aplicados a titulos y estilos

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 // SWIPER 
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
@@ -24,65 +23,23 @@ const swiper = new Swiper('.swiper', {
         shadowOffset: 10,
         shadowScale: 0.0001,
     },
-    speed: 2000,
+    speed: 1000,
     loop: true,
 });
 
-let test = document.querySelector(".swiper-slide-active");
-
-let hijos = test.children;
-
-let source = hijos[0].getAttribute('src')
-
-let elementos = document.getElementById('Elemento')
-
-elementos.src= source
-
-// Iterar sobre los hijos y mostrarlos en la consola
-//for (var i = 0; i < hijos.length; i++) {
-//  console.log("Hijo " + (i + 1) + ": " + hijos[i].tagName);
-//console.log(source);
-//}
-
-function a(){
-
-setTimeout(cambioImagen,150)
-
-}
-
-function cambioImagen(){
-
-let test = document.querySelector(".swiper-slide-active");
-
-let hijos = test.children;
-
-let source = hijos[0].getAttribute('src')
-
-let elementos = document.getElementById('Elemento')
-
-elementos.src= source
-}
-
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    a()
- });
-});
-
-// Configurar las opciones del observer
-var config = { attributes: true, childList: true, subtree: true };
-
-// Empezar a observar el DOM
-observer.observe(document.body, config);
 
 
-const text = document.querySelector('.titleTokio');
 
-const textLife = document.querySelector('.titleLife');
+//ANIMACIONES TEXTO 
 
-const textNight = document.querySelector('.titleStory');
+var text = document.querySelector('#Tokio');
 
-const textStory = document.querySelector('.titleNight');
+var textLife = document.querySelector('#Life');
+
+var textStory = document.querySelector('#Story');
+
+var textNight = document.querySelector('#Nights'); 
+
 
 
 // TAKING THE CHARS
@@ -102,64 +59,154 @@ const splitTextNight = strTextNight.split("");
 
 const splitTextStory = strTextStory.split("");
 
-console.log(splitText, splitTextLife, splitTextNight, splitTextStory)
+console.log(textNight)
 
 
+text.textContent="";
 
+textLife.textContent="";
+
+//textNight.textContent="";
+
+//textStory.textContent="";
 
 for(let i = 0; i < splitText.length; i++){
-    text.innerHTML+= "<span>" + splitText[i] +"</span>"
-}
 
+    text.innerHTML+= '<span class="lifes">' + splitText[i] +'</span>'
+}
 
 for(let i = 0; i <splitTextLife.length; i++){
     textLife.innerHTML+= "<span>" + splitTextLife[i] + "</span>"
 }
 
+for(i=0; i < splitTextStory.length; i++){
+     textStory.innerHTML+= "<span>" + splitTextStory[i] +"</span>"
+}
 
-//
-//    textNight.innerHTML+= "<span>" + splitText[i] +"</span>"
-//    textStory.innerHTML+= "<span>" + splitText[i] +"</span>"
+for(i=0; i< splitTextNight.length; i++){
+    textNight.innerHTML+= "<span>" + splitTextNight[i] +"</span>"
+}
 
-
+// FUNCION ONTICK() TITULO LIFESTYLE
 
 let charLife = 0; 
-let intervalLife = setInterval(onTickLife, 100)
+let intervalLife = setInterval(onTickLife, 400)
 
 function onTickLife(){
-const spanLife = textLife.querySelectorAll('span')[charLife];
-    spanLife.classList.add('fade');
-    console.log(splitTextLife)
-    charLife++
-    if(charLife === splitTextLife.length){
-        completeLife();
-    return
-}
+    const spanLife = textLife.querySelectorAll('span')[charLife];
+        spanLife.classList.add('fade');
+        textLife.style.color="white"
+        charLife++
+        if(charLife === splitTextLife.length){
+            //console.log(intervalLife)
+            completeLife();
+            return
+    }
 }
 
 function completeLife(){
-clearInterval(intervalLife)
-intervalLife = null;
+    clearInterval(intervalLife)
+    intervalLife = null;
 }
 
-
+// FUNCION ONTICK() TITULO TOKIO
 
 let char = 0;
-
-let interval = setInterval(onTick, 80)
+let interval = setInterval(onTick, 100)
 
 function onTick(){
 const span = text.querySelectorAll('span')[char];
-    span.classList.add('fade');
-    char++
+        span.classList.add('fade');
+        char++
     if(char === splitText.length){
         complete();
-    return
+        return
 }
 }
 
 function complete(){
-clearInterval(interval)
-interval = null;
+    clearInterval(interval)
+    interval = null;
+
 }
 
+let test = document.querySelector(".swiper-slide-active");
+
+let hijos = test.children;
+
+let source = hijos[0].getAttribute('src')
+
+let elementos = document.getElementById('Elemento')
+
+elementos.src= source
+
+
+function a(){
+
+setTimeout(cambioImagen,150)
+
+}
+
+let  cambioImagen = ()=> {
+
+const test = document.querySelector(".swiper-slide-active");
+
+let hijos = test.children;
+
+let source = hijos[0].getAttribute('src');
+
+var alt = hijos[0].getAttribute('alt')
+
+let elementos = document.getElementById('Elemento')
+
+elementos.src= source
+
+cambioTitulo(alt)
+
+}
+
+var observer = new MutationObserver(function(mutations) {
+    a();
+});
+
+//  opciones del observer
+var config = { attributes: true, childList: true, subtree: true };
+
+// observar el DOM
+observer.observe(document.body, config);
+
+// DESAPARECER TITULOS CUANDO NO SON LLAMADOS
+
+textLife.style.display="none";
+textNight.style.display="none";
+textStory.style.display="none"; 
+
+function b (){
+setTimeout(cambioTitulo,150)
+}
+
+let cambioTitulo = (alt) => {
+
+text.style.display="none";
+textLife.style.display="none";
+textNight.style.display="none";
+textStory.style.display="none";
+
+
+switch (alt) {
+        case 'TokioNight':
+            text.style.display = "inline-block";
+            break;
+        case 'TokioOne':
+            textLife.style.display = "inline-block";
+            break;
+        case 'TokioN':
+            textNight.style.display = "inline-block";
+            break;
+        default:
+            textStory.style.display = "inline-block";
+            break;
+    }
+}
+
+//console.log(text)
